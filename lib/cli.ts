2d0a01c3af61f58636c7f2d671c5bccac754ed15
Joseph Snell <joepsnell@gmail.com>
2020-06-10 17:52:26 -0500

    update cli interface

--- lib/cli.ts
+++ lib/cli.ts
@@ -6,4 +6,18 @@ export class CLI extends Construct {
   constructor(options: CLIOptions = {}) {
     super(undefined as any, '');
   }
+  
+  public parse(): void {
+    validate(this);
+  }
+}
+
+function validate(app: App) {
+
+    // Note this is a copy-paste of https://github.com/aws/constructs/blob/master/lib/construct.ts#L438.
+    const errors = Node.of(app).validate();
+    if (errors.length > 0) {
+      const errorList = errors.map(e => `[${Node.of(e.source).path}] ${e.message}`).join('\n  ');
+      throw new Error(`Validation failed with the following errors:\n  ${errorList}`);
+    }
 }
